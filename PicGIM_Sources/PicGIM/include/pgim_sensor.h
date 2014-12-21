3fd106b057fa6c34a5714f00b4ce085c46b6dde4
gk-skymatrix <gk-skymatrix@users.noreply.github.com>
2014-12-21 05:50:42 +0100

    SENSOR: Work on documentation.

--- PicGIM_Sources/PicGIM/include/pgim_sensor.h
+++ PicGIM_Sources/PicGIM/include/pgim_sensor.h
@@ -80,7 +80,7 @@
 		_pg_float	pg_sensor_volt_adc_ref		( _pg_float volt_stable_ref );
 
 		/*!
-			\brief			This function measures the temperature value of NTC sensor.
+			\brief			This function measures the temperature value of an NTC sensor.
 			\param			\b ad_channel : channel where is connected the ntc to measure on AD input. \n
 							Must be specified in ad_converter style, like as PG_CH_0, PG_CH_1, PG_CH_2... \n
 							This module will only execute the calls pg_adc_start() and pg_adc_get(). \n
@@ -111,48 +111,89 @@
 		This module depends on the module AD_CONVERTER which must then be configured in function \n
 		of the requests of the user (for example: the number of channels to be dedicated to the sensors). \n
 		In the bottom of the page there is a code example to use.\n
-		This module will only execute, to AD_CONVERTER module the calls: pg_adc_start() and pg_adc_get(). \n
+		This module will only execute the AD_CONVERTER module calls: pg_adc_start() and pg_adc_get(). \n
 		The name of the channel to be passed to functions must be in AD_CONVERTER style, \n
 		like as \ref PG_CH_0 , \ref PG_CH_1 , \ref PG_CH_2 ... \n \n
-
-		\section	confsens	Module configuration
+			
+		\section	sensormodconf	Module configuration
 
 			\htmlonly <hr> \endhtmlonly
-			<p>
 			In order to use this module, it must be enabled and configured in: \n
-				\arg \b pgim_module_setup_public.h : the main management file for the modules inclusion in the project. \n \n
-				\arg \b pgim_sensor_setup_public.h : the management file for the sensor module configuration. \n \n
-			</p>
-			\n\n\n 
-
-			\subsection	confsensadref	Volt AD Ref   (Vdd measurement)
-				\htmlonly <hr> \endhtmlonly
-				Text here.
-				\n \n \n
-				
-			\subsection	confsensntc		Ntc
-				\htmlonly <hr> \endhtmlonly
-				Text here.
-				\n \n \n
-			
-			\subsection	confsenshw		Typology circuit to be used
-				\htmlonly <hr> \endhtmlonly
-				\image html	ntc.png
-				\n \n \n
-				
-			\subsection	confsenscode	Example code
-				\htmlonly <hr> \endhtmlonly
-				
+			\arg \b pgim_module_setup_public.h : the main management file for the modules inclusion in the project. \n
 			\code
-			void main( void ) {
-				float myflo;
-				pg_initialize();
-				pg_adc_set( PG_ANALOG_CHANNELS_PARAM , PG_1_CHANNEL );
-				pg_adc_set( PG_ADC_MODULE , PG_ON );
-				myflo = pg_sensor_ntc( PG_CH_0 );
-				PG_INFINITE_LOOP;
-			}
+				#define PGIM_SENSOR					PG_ENABLE
 			\endcode
+			This define enables or disables the module.<br>
+			\htmlonly
+			It must be:<br>
+			\endhtmlonly
+			\arg \b PG_ENABLE : Sensor module enabled.
+			\arg \b PG_DISABLE : Sensor module disabled.
+			\n \n \n
+			\arg \b pgim_sensor_setup_public.h : the management file for the sensor module configuration. \n
+			\code
+				#define PGIM_SENSOR_ADC_REF			PG_ENABLE
+			\endcode
+			This define enables or disables the sensor Volt AD Ref.<br>
+			\htmlonly
+			It must be:<br>
+			\endhtmlonly
+			\arg \b PG_ENABLE : Sensor enabled.
+			\arg \b PG_DISABLE : Sensor disabled.
+			\n \n \n
+			In the same way are enabled and disabled all other sensors.
+			\n \n \n 
+
+			\subsection	sensorvoltadref		Volt AD Ref   (VDD measurement)
+				\htmlonly <hr> \endhtmlonly
+				This is not really a sensor. It simply acquires the value of a stable voltage via input of an AD converter. \n
+				The acquired value, since it is stable, and then fixed, does not carry directly any information, \n
+				but through it, the reference of the AD converter can be measured. \n
+				For the module AD_CONVERTER, the Vref reference is the power supply voltage of the microcontroller, or VDD. \n \n				
+				\b Example \b circuit \b schematic: \n
+				\image html	sensor_voltadref.png
+				In this circuit schematic, for example, a 2.5V reference voltage is used. \n
+				Of course, nothing prevents from using other values. \n
+				It is important that the \b exact \b value of voltage generated is measured and written in the relative configuration file \n
+				specified in the initial section of this module documentation. \n \n
+				\b Function: \n
+				\arg \b pg_sensor_volt_adc_ref() : This function measures the value of the reference voltage of the AD converter. \n \n 
+				\b Example \b code: \n
+				\code
+					void main( void ) {
+						float real_pic_vdd;
+						pg_initialize();
+						pg_adc_set( PG_ANALOG_CHANNELS_PARAM , PG_1_CHANNEL );
+						pg_adc_set( PG_ADC_MODULE , PG_ON );
+						real_pic_vdd = pg_sensor_volt_adc_ref( 2.5 );
+						PG_INFINITE_LOOP;
+					}
+				\endcode
+				\n \n \n
+				
+			\subsection	sensorntc		NTC 
+				\htmlonly <hr> \endhtmlonly
+				This sensor allows to measure the temperature via a resistor NTC connected as shown in the image below. \n
+				The values of the circuit components must be declared in the configuration files specified in the \n
+				initial section of this module documentation. \n \n
+				
+				\b Typology \b circuit \b to \b be \b used: \n
+				\image html	sensor_ntc.png
+				\b Function: \n
+				\arg \b pg_sensor_ntc() : This function measures the temperature value of an NTC sensor. \n \n 
+				\b Example \b code: \n
+				\code
+					void main( void ) {
+						float temperature;
+						pg_initialize();
+						pg_adc_set( PG_ANALOG_CHANNELS_PARAM , PG_1_CHANNEL );
+						pg_adc_set( PG_ADC_MODULE , PG_ON );
+						temperature = pg_sensor_ntc( PG_CH_0 );
+						PG_INFINITE_LOOP;
+					}
+				\endcode
+				
+				\n \n \n
 			
 		\htmlonly <br><br> \endhtmlonly
 
